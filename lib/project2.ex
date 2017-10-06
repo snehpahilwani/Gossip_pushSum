73874913c539d5e203f6008de851a2c3bc37eae1
Purnendu <purnendu@Purnendu.local>
2017-10-05 21:52:26 -0400

    spawning worker nodes from input parameters

--- lib/project2.ex
+++ lib/project2.ex
@@ -2,5 +2,22 @@ defmodule Project2 do
   def main(args) do
     IO.puts "Hello world"
     IO.puts args
+    serveOrWork = "#{args}"
+    {k, _} = Integer.parse(serveOrWork)
+    loop(k)
   end
+
+
+  def loop(max) do
+    if max<1 do
+        # IO.puts "Num : #{max}"
+    else
+        IO.puts "Num : #{max}"
+        pid = spawn(WORKER, :talktoworkers, [max])
+        IO.inspect pid
+        loop(max-1)
+    end
+  end 
+
+
 end
---
+++ lib/worker.ex
@@ -0,0 +1,11 @@
+defmodule Worker do
+
+  def talktoworkers(k) do
+    IO.puts "wrkr"
+        # receive do
+        #     msg ->
+        #         IO.puts "Bitcoin from worker : "
+        # end
+    end 
+
+end
